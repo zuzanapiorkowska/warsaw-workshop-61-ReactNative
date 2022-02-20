2c33f1009d692b373efcbab9dfb1f5ee64bf22ea
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-20 12:06:43 +0100

    add useState, useCallback, UseEffect

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { Button, Image, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [state, setState] = useState(false);
+  const handleOnPress = useCallback(() => {
+    console.log("handle Press");
+    if (state === false) {
+      setState(true);
+    } else if (state===true){
+      setState(false);
+    }
+  }, [state]);
+
+  useEffect(()=> {
+    console.log("jestem wyrenderowany");
+  }, [state]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {state === true ? <Text>clicked</Text> : <Text>not clicked</Text>}
+      <ImageBackground source={{ uri: "https://wallpapercave.com/wp/wp2561075.jpg" }} style={{ width: 200, height: 200 }} resizeMode="cover" >
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" backgroundColor="pink" />
+        <Button title="first Button" onPress={handleOnPress} />
+        <Image source={{ uri: "https://warsawjs.com/static/images/ogp-v1.png", height: 150, width: 300 }} />
+        <TextInput placeholder='Text...' backgroundColor="pink"></TextInput>
+      </ImageBackground>
     </View>
   );
 }
